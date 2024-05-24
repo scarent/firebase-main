2ef380bedae11e829c6cee29316224f07b0b1f9b
scarent <jorgeee.g.a@gmail.com>
2024-05-24 09:06:25 -0400

    tabla
    logre hacer la tabla

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
-import { registrarpersona } from "./promesas.js";
+import { obtenerPersonas, registrarpersona } from "./promesas.js";
 
 window.addEventListener("load",()=>{
-    document.getElementById("btnregistrar").addEventListener("click",registrar)
+    document.getElementById("btnregistrar").addEventListener("click",registrar);
+    cargarDatos();
 });
 const registrar = ()=>{   //variable flecha para gatillar una funcion
     //recupero el elemento
@@ -17,15 +18,35 @@ const registrar = ()=>{   //variable flecha para gatillar una funcion
     let vrut = erut.value;
     let vcorreo = ecorreo.value;
     //creo el objeto
-    let objeto = {nombre:vnombre, apellido:vapellido,fecha:vfechanac,rut:vrut,correo:vcorreo};
+    let objeto = {nombre:vnombre, apellido:vapellido,fechaNacimiento:vfechanac,rut:vrut,correo:vcorreo};
     console.log(objeto);
     //validacion del objeto
     registrarpersona(objeto).then(()=>{
         alert("registrado con éxito");
+        cargarDatos();
     }).catch((r)=>{
         alert("algo ocurrio");
         alert(r);
     });
-    
+}
+const cargarDatos = ()=>{
+    // ir a la base d datos a buscar lo registrado
+    obtenerPersonas().then((personas)=>{
+        console.log("recupere")
+        console.log(personas)
+        let estructura = "";
+        personas.forEach((persona)=>{
+            estructura += "<tr>";
+            estructura += "<td>"+persona.nombre+"</td>";
+            estructura += "<td>"+persona.apellido+"</td>";
+            estructura += "<td>"+persona.rut+"</td>";
+            estructura += "<td>"+persona.correo+"</td>";
+            estructura += "<td>"+persona.fechaNacimiento+"</td>";
+            estructura += "<td>"+persona.edad+"</td>";
+            estructura += "</tr>"
+        });
+        document.getElementById("tbDatos").innerHTML = estructura;
+
+    });
 
 }
