e69591233f9d9766681edd67a66335b8fee8e816
scarent <jorgeee.g.a@gmail.com>
2024-05-24 10:10:50 -0400

    botones
    logre hacer el boton actualizar y actualiza bbn

--- assets/promesas.js
+++ assets/promesas.js
@@ -1,4 +1,4 @@
-import {collection,addDoc, getDocs} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import {collection,addDoc, getDocs,doc,updateDoc} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { db } from "./firebase.js";
  export const registrarpersona= async(persona)=>{
     console.log("persona")
@@ -14,7 +14,11 @@ export const obtenerPersonas = async()=>{
     quearySnap.forEach(doc => {
         //console.log(doc.id);  //recupero id de los documentos
         //console.log(doc.data());
-        listado.push(doc.data());
+        listado.push({...doc.data(),id:doc.id}); //le agrega un id a los objetos
     });
     return listado;
+}
+export const actualizarPersona = async(objeto,id)=>{
+    const ref = doc(db,"persona",id);
+    await updateDoc(ref,objeto);
 }
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,9 @@
-import { obtenerPersonas, registrarpersona } from "./promesas.js";
+import { actualizarPersona, obtenerPersonas, registrarpersona } from "./promesas.js";
 
 window.addEventListener("load",()=>{
     document.getElementById("btnregistrar").addEventListener("click",registrar);
+    document.getElementById("btnActualizar").addEventListener("click",actualizar);
+    
     cargarDatos();
 });
 const registrar = ()=>{   //variable flecha para gatillar una funcion
@@ -11,14 +13,16 @@ const registrar = ()=>{   //variable flecha para gatillar una funcion
     let efechanac = document.getElementById("fecha_nacimiento");
     let erut = document.getElementById("rut");
     let ecorreo = document.getElementById("correo");
+    let eedad = document.getElementById("edad");
     //recupero el valor
     let vnombre= enombre.value;
     let vapellido = eapellido.value;
     let vfechanac = efechanac.value;
     let vrut = erut.value;
     let vcorreo = ecorreo.value;
+    let vedad = eedad.value;
     //creo el objeto
-    let objeto = {nombre:vnombre, apellido:vapellido,fechaNacimiento:vfechanac,rut:vrut,correo:vcorreo};
+    let objeto = {nombre:vnombre, apellido:vapellido,fechaNacimiento:vfechanac,rut:vrut,correo:vcorreo,edad:vedad};
     console.log(objeto);
     //validacion del objeto
     registrarpersona(objeto).then(()=>{
@@ -43,10 +47,56 @@ const cargarDatos = ()=>{
             estructura += "<td>"+persona.correo+"</td>";
             estructura += "<td>"+persona.fechaNacimiento+"</td>";
             estructura += "<td>"+persona.edad+"</td>";
+            estructura += "<td><button id='UPD"+persona.id+"'>Actualizar</button></td>"; //boton para actualizar
+            estructura += "<td><button id='DEL'"+persona.id+"'>Eliminar</button></td>";  //boton para delete
             estructura += "</tr>"
         });
         document.getElementById("tbDatos").innerHTML = estructura;
-
+        personas.forEach((persona)=>{
+            let botonUPD = document.getElementById("UPD"+persona.id);
+            botonUPD.addEventListener("click",()=>{
+                let enombre = document.getElementById("UPDnombre");
+                let eapellido = document.getElementById("UPDapellido");
+                let efechanac = document.getElementById("UPDfecha_nacimiento");
+                let erut = document.getElementById("UPDrut");
+                let ecorreo = document.getElementById("UPDcorreo");
+                let eedad = document.getElementById("UPDedad");
+                enombre.value = persona.nombre;
+                eapellido.value = persona.apellido;
+                erut.value = persona.rut;
+                ecorreo.value = persona.correo;
+                efechanac.value = persona.fechaNacimiento;
+                eedad.value = persona.edad;
+                document.getElementById("btnActualizar").value = persona.id;
+            })
+        })
     });
 
+}
+const actualizar = ()=>{   //variable flecha para gatillar una funcion
+    //recupero el elemento
+    let enombre = document.getElementById("UPDnombre");
+    let eapellido = document.getElementById("UPDapellido");
+    let efechanac = document.getElementById("UPDfecha_nacimiento");
+    let erut = document.getElementById("UPDrut");
+    let ecorreo = document.getElementById("UPDcorreo");
+    let eedad = document.getElementById("UPDedad");
+    //recupero el valor
+    let vnombre= enombre.value;
+    let vapellido = eapellido.value;
+    let vfechanac = efechanac.value;
+    let vrut = erut.value;
+    let vcorreo = ecorreo.value;
+    let vedad = eedad.value;
+    //creo el objeto
+    
+    let objeto = {nombre:vnombre, apellido:vapellido,fechaNacimiento:vfechanac,rut:vrut,correo:vcorreo,edad:vedad};
+    let id =document.getElementById("btnActualizar").value;
+    console.log(objeto);
+    actualizarPersona(objeto,id).then(()=>{
+        alert("se actualizo con exito");
+        cargarDatos();
+
+    });
+    
 }
